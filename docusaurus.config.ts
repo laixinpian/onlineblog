4f43938910dda40f56f1e80f32ba0b2195622756
tanggong <yataoboy@gmail.com>
2024-07-09 17:45:14 +0800

    fix: url sub path.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://laixinpian.github.io/onlineblog/',
+  url: 'https://laixinpian.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/onlineblog',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
